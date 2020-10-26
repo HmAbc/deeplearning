b8d60e46ea7d629f931094efad2526d42dcdd08e
HmAbc <houminghua3521@gmail.com>
2020-10-26 21:48:07 +0800

    pyplot learning

---
+++ chapter1/pyplot-fun.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy
+from matplotlib import pyplot
+
+x = numpy.arange(0, 6, 0.1)
+y = numpy.sin(x)
+y2 = numpy.cos(x)
+
+#绘制图形
+pyplot.plot(x, y, label='sin')
+# pyplot.plot(x, y2, lable="cos")
+# pyplot.xlabel('x')
+# pyplot.ylabel('y')
+# pyplot.title('sin&cos')
+# pyplot.legend()
+pyplot.show()
