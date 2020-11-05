32bd8698c85bb3982497cc69e8439c6d7fd33ecd
HmAbc <houminghua3521@gmail.com>
2020-11-05 20:50:19 +0800

    logic

--- chapter2/logic_circus.py
+++ chapter2/logic_circus.py
@@ -32,9 +32,31 @@ def AND(x1, x2):
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])  # 只有权重不一样
-    b = -0.7
+    b = 0.7
     temp = np.sum(w*x) + b
     if temp <= 0:
         return 0
     else:
         return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    temp = np.sum(w*x) + b
+    if temp <= 0:
+        return 0
+    else:
+        return 1
+
+# 异或门的实现，由其他感知机叠加而成
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1,x2)
+    y = AND(s1, s2)
+    return y
+
+# print(XOR(0, 0))
+# print(XOR(0, 1))
+# print(XOR(1, 0))
+# print(XOR(1, 1))
